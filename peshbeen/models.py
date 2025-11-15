a08d531f1b2a56e90fd66e19b9693c7c0a9da24b
Mustafa <mustafaslan63@gmail.com>
2025-11-15 16:05:00 +0000

    n_aicc

--- peshbeen/models.py
+++ peshbeen/models.py
@@ -1559,26 +1559,26 @@ class MsHmmRegression:
     @property
     def aic(self):
         # k = self.N * self.X.shape[1] + self.N ** 2 + self.N - 1 # number of parameters: regression coeffs + transition matrix + initial state probs
-        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1) # number of parameters: regression coeffs + transition matrix + initial state probs
+        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1) + self.N # number of parameters: regression coeffs + transition matrix + initial state probs + variance per state
         return 2 * k - 2 * self.LL
 
     @property
     def aicc(self):
-        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1) # number of parameters: regression coeffs + transition matrix + initial state probs
+        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1) + self.N # number of parameters: regression coeffs + transition matrix + initial state probs
         n = self.T  # effective number of observations
         return self.aic + (2 * k * (k + 1)) / (n - k - 1)
 
     @property
     def bic(self):
         
-        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1)
+        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1) + self.N
         
         n = self.T  # effective number of observations
         return -2 * self.LL + k * np.log(n)
     
     @property
     def hqc(self):
-        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1)
+        k = self.N * self.X.shape[1] + self.N * (self.N - 1) + (self.N - 1) + self.N
         n = self.T  # effective number of observations
         return -2 * self.LL + 2 * k * np.log(np.log(n))
 
