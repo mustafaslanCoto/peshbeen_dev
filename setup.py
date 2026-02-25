ff9d2a6d940ccf0d07c2cad36f58c0256d0230a2
Mustafa Aslan <mustafaslan63@gmail.com>
2026-02-25 01:12:43 +0000

    py314

--- setup.py
+++ setup.py
@@ -1,31 +1,44 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3' 
-DESCRIPTION = 'Auto regressive bagging and boosting package'
-LONG_DESCRIPTION = 'Forecating using historical data as predictors'
+VERSION = "0.0.4"
+DESCRIPTION = "Auto regressive bagging and boosting package"
+LONG_DESCRIPTION = "Forecasting using historical data as predictors"
 
-# Setting up
 setup(
-        name="peshbeen", 
-        version=VERSION,
-        author="Mustafa Aslan",
-        author_email="<mustafaslan63@email.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        url='https://github.com/mustafaslanCoto/peshbeen',
-        install_requires=["xgboost", "lightgbm", "catboost", "pandas",
-                          "numpy", "scikit-learn", "datetime", "hyperopt","statsmodels", "seaborn", "statsforecast",
-                          "matplotlib", "window_ops", "cubist", "scipy", "numba", "great_tables"], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
-        keywords=['python', 'peshbeen', 'forecasting', 'time series', 'machine learning'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Data Scientists",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ]
+    name="peshbeen",
+    version=VERSION,
+    author="Mustafa Aslan",
+    author_email="mustafaslan63@email.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    url="https://github.com/mustafaslanCoto/peshbeen",
+    install_requires=[
+        "numpy>=2.0",
+        "pandas>=2.2",
+        "scikit-learn>=1.5",
+        "scipy>=1.15",
+        "statsmodels>=0.14",
+        "matplotlib>=3.9",
+        "seaborn>=0.13",
+        "hyperopt>=0.2.7",
+        "xgboost>=2.0",
+        "lightgbm>=4.0",
+        "catboost>=1.2",
+        "cubist>=1.0.0",
+        "statsforecast>=1.7",
+        "window-ops>=0.0.15",
+        "numba>=0.60",
+        "great-tables>=0.10",
+    ],
+    python_requires=">=3.10",
+    keywords=["python", "peshbeen", "forecasting", "time series", "machine learning"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.14",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
 )
